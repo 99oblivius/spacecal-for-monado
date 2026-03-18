b0be9e38ac238c0cd33622c52aaca9e19f897009
99oblivius <99dark.oblivion99@gmail.com>
2026-03-18 15:19:49 +0100

    remove WiVRn fallback + use packages.default + proper asset install + remove .envrc and flake.lock

--- flake.nix
+++ flake.nix
@@ -23,8 +23,8 @@
         lib = pkgs.lib;
 
         naersk' = pkgs.callPackage naersk {};
-      in rec {
-        defaultPackage = naersk'.buildPackage {
+      in {
+        packages.default = naersk'.buildPackage {
           src = ./.;
           buildInputs = with pkgs; [
             openxr-loader
@@ -39,30 +39,23 @@
               --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [pkgs.openxr-loader]}" \
               --prefix LD_LIBRARY_PATH : "/run/opengl-driver/lib" \
               --run '
-                  if [ -z "$XR_RUNTIME_JSON" ]; then
-                      ACTIVE_RUNTIME="$HOME/.config/openxr/1/active_runtime.json"
-                      if [ -f "$ACTIVE_RUNTIME" ]; then
-                          export XR_RUNTIME_JSON="$ACTIVE_RUNTIME"
-                      else
-                          ACTIVE_RUNTIME="/etc/xdg/openxr/1/active_runtime.json"
-                          if [ -f "$ACTIVE_RUNTIME" ]; then
-                              export XR_RUNTIME_JSON="$ACTIVE_RUNTIME"
-                          else
-                              export XR_RUNTIME_JSON="${pkgs.wivrn}/share/openxr/1/openxr_monado.json"
-                          fi
-                      fi
-                  fi
+                if [ -z "$XR_RUNTIME_JSON" ]; then
+                  for f in \
+                    "$HOME/.config/openxr/1/active_runtime.json" \
+                    "/etc/xdg/openxr/1/active_runtime.json" \
+                    "${pkgs.monado}/share/openxr/1/openxr_monado.json"; do
+                    if [ -f "$f" ]; then export XR_RUNTIME_JSON="$f"; break; fi
+                  done
+                fi
               '
 
-              mkdir -p $out/share/applications
-              mkdir -p $out/share/icons
-
-              cp data/dev.oblivius.spacecal-for-monado.svg $out/share/icons/
-              cp data/dev.oblivius.spacecal-for-monado.desktop $out/share/applications/
+            install -Dm644 data/dev.oblivius.spacecal-for-monado.desktop $out/share/applications/dev.oblivius.spacecal-for-monado.desktop
+            install -Dm644 data/dev.oblivius.spacecal-for-monado.svg $out/share/icons/hicolor/scalable/apps/dev.oblivius.spacecal-for-monado.svg
+            install -Dm644 data/dev.oblivius.spacecal-for-monado.metainfo.xml $out/share/metainfo/dev.oblivius.spacecal-for-monado.metainfo.xml
           '';
         };
 
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             alejandra
             rust-analyzer
@@ -73,8 +66,6 @@
               "rustc"
               "rustfmt"
             ])
-
-            # SCFM specific dependencies
             openxr-loader
             libadwaita
             gtk4
